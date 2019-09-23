2bd928f9611b98d19a19f6386a45b4ed6f47be0c
Daniel Voicu <daniel.voicu@brandfusion.ro>
2019-09-24 01:13:05 +0300

    updated

--- configuration.js
+++ configuration.js
@@ -1,6 +1,9 @@
+const siblings = n => [...n.parentElement.children].filter(c=>c.nodeType == 1 && c!=n);
 var buttons= [...document.querySelectorAll("#configuration button")];
 buttons.map(el=>{
   el.addEventListener("click", (e)=>{
+    siblings(e.currentTarget).map(el=> el.classList.remove("active"));
+    e.currentTarget.classList.add("active");
     document.querySelector(".paragraph-content").classList = "paragraph-content";
     document.querySelector(".paragraph-content").classList = `paragraph-content ${e.currentTarget.value}`;
   });
